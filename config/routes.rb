58353ad604f8aa749eb2e679835504dbbc6e7bf1
John Schlesinger <jschles1@ramapo.edu>
2017-05-12 23:14:09 +0000

    Made small syntax change to omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: 'login'
   get '/logout', to: 'sessions#destroy', as: 'logout'
 
-  get '/auth/facebook/callback' => 'sessions#create'
+  get '/auth/facebook/callback', to: 'sessions#create'
 
 end
