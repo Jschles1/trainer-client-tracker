a2ca0e582a9fe6cf54717141bc58a0e7b5372c49
John Schlesinger <jschles1@ramapo.edu>
2017-05-12 22:42:39 +0000

    Added appointment validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :client
+
+  validates :date, numericality { greater_than: Time.now }
+  validates :date, presence: true
+  validates :date, uniqueness: true
 end
