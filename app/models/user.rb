d5c98be780314b4f63b3c549f923ea60cc07c44f
John Schlesinger <jschles1@ramapo.edu>
2017-05-12 03:32:32 +0000

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :clients, through: :appointments
 
   has_secure_password
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
