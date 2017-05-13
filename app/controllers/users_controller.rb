28d24e1ee2d5f8bb400f6ad089dc9dfde9a0b244
John Schlesinger <jschles1@ramapo.edu>
2017-05-13 00:27:55 +0000

    Completed new & create actions, added user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def show
@@ -12,5 +20,11 @@ class UsersController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
