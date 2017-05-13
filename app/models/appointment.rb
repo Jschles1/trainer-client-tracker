44ac480358d6efd7172b899456d461145949fcc0
John Schlesinger <jschles1@ramapo.edu>
2017-05-13 23:51:42 +0000

    Reformatted file

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :client
-
   validates :date, numericality { greater_than: Time.now }
   validates :date, presence: true
   validates :date, uniqueness: true
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,6 @@
 class Client < ApplicationRecord
   has_many :appointments
   has_many :users, through: :appointments
-
   validates :name, :email, :phone, :age, :weight, :goal, presence: true
   validates :email, uniqueness: true
   validates :age, numericality: { greater_than: 0, less_than: 100 }
