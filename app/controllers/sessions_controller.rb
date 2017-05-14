d509f37decc1ae5d7c6a97216e382e9bfc4a1deb
John Schlesinger <jschles1@ramapo.edu>
2017-05-14 00:33:22 +0000

    Added oauth_login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,16 @@ class SessionsController < ApplicationController
   def auth
     request.env['omniauth.auth']
   end
+
+  def oauth_login
+    @user = User.find_or_create_by(:uid => auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.provider = auth['provider']
+      u.oauth_token = auth['credentials']['token']
+      u.oauth_expires_at = auth['credentials']['expires_at']
+    end
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
 end
