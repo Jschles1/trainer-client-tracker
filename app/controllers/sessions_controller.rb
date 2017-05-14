57f6e50ecd820129a08e1ed7d4fd9e980f23e662
John Schlesinger <jschles1@ramapo.edu>
2017-05-14 00:19:20 +0000

    Added placeholder code to create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if !params[:provider].nil?
+      oauth_login
+    elsif # If email and/or password are left blank:
+      # ...
+    else
+      normal_login
+    end
   end
 
   def destroy
