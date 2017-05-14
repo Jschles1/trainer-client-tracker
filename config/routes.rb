7c554f463c25d63f871224f87b39b792732eef2a
John Schlesinger <jschles1@ramapo.edu>
2017-05-14 01:21:32 +0000

    Added sessions resources macro for create

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :clients
+  resources :sessions, only: [:create]
 
   get '/signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
