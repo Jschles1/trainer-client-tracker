a685a10149b60953b5bd4a0e6da0fa0468d171d3
John Schlesinger <jschles1@ramapo.edu>
2017-05-14 01:09:18 +0000

    Added resource macros for users & clients

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'static#home'
 
+  resources :users
+  resources :clients
+
   get '/signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
   get '/logout', to: 'sessions#destroy', as: 'logout'
