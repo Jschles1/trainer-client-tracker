16230761b0daf23f60ed490f592b7da223e75a65
John Schlesinger <jschles1@ramapo.edu>
2017-05-15 23:36:18 +0000

    Fixed small syntax error

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :client
-  validates :date, numericality { greater_than: Time.now }
+  validates :date, numericality: { greater_than: Time.now }
   validates :date, presence: true
   validates :date, uniqueness: true
 end
