486327d093cd63ee2d58e690d6987cd543921d19
John Schlesinger <jschles1@ramapo.edu>
2017-05-16 01:41:41 +0000

    Changed redirects to clients_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -35,7 +35,7 @@ class SessionsController < ApplicationController
     end
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to clients_path
     else
       # redirect to login form with errors
     end
@@ -45,7 +45,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
     if !!@user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to clients_path
     else
       # redirect to login form with errors
     end
