38058037b9771d622f3a2d5b9aa66d63e2b562f9
John Schlesinger <jschles1@ramapo.edu>
2017-05-16 00:41:06 +0000

    Moved user_authorized to application_helper, changed current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,16 +29,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
-  def user_authorized?
-    if !logged_in?
-      redirect_to login_path
-    else
-      @user = User.find(params[:id])
-      if @user.id != current_user.id
-        redirect_to user_path(current_user)
-        flash[:alert] = "Sorry, you aren't authorized to view this page."
-      end
-    end
-  end
 end
