e11376052d143cbab8a699e5fd999f22479f8f42
John Schlesinger <jschles1@ramapo.edu>
2017-05-23 01:08:02 +0000

    Added custom email validator to user & client models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,11 +1,13 @@
 class Client < ApplicationRecord
+  include ActiveModel::Validations
+
   has_many :appointments
   has_many :users, through: :appointments
 
   validates :name, :email, :phone, :age, :weight, :goal, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, email: true
   validates :age, numericality: { greater_than: 0, less_than: 100 }
   validates :weight, numericality: { greater_than: 0 }
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates_format_of :phone, :with => /\d{3}-\d{3}-\d{4}/, :on => :create
 end
