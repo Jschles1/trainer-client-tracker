f97446da1e6324fd90b09ad794d6bdd25a6bde82
John Schlesinger <jschles1@ramapo.edu>
2017-05-30 01:02:58 +0000

    Added client attributes writer method

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,4 +5,9 @@ class Appointment < ApplicationRecord
   validates_datetime :date, :on => :create, :on_or_after => :today
   validates :date, presence: true
   validates :date, uniqueness: true
+
+  def client_attributes=(client_attributes)
+    self.client = Client.find_or_create_by(name: client_attributes[:name])
+    self.client.update(client_attributes)
+  end
 end
