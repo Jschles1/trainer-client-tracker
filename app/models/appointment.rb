0ce9bde934f0f6a5e860952c400d328c40d26dbe
John Schlesinger <jschles1@ramapo.edu>
2017-05-31 01:29:43 +0000

    modified client_attributes= method

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,10 +7,9 @@ class Appointment < ApplicationRecord
   validates :date, uniqueness: true
 
   def client_attributes=(client_attributes)
-    # self.client = Client.find_or_create_by(name: client_attributes[:name])
-    # self.client.update(client_attributes)
     if client_attributes[:name] != ""
       self.client = Client.find_or_create_by(client_attributes)
+      self.client.update(client_attributes)
     end
   end
 end
