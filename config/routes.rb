0fa1c068c9a433c617980e03a1965506dc899b28
John Schlesinger <jschles1@ramapo.edu>
2017-06-02 00:24:01 +0000

    Added appointment_complete route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :appointments
   resources :sessions, only: [:create]
 
+  get 'clients/:id/appointment_complete', to: 'clients#appointment_complete' as: 'appointment_complete' 
+
   get '/signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
   get '/logout', to: 'sessions#destroy', as: 'logout'
