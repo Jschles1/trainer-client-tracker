750529938e4168559d2143cddd78a580bb9bcba8
John Schlesinger <jschles1@ramapo.edu>
2017-06-05 04:36:14 +0000

    Deleted create & new actions

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,21 +3,6 @@ class AppointmentsController < ApplicationController
     @appointments = current_user.appointments
   end
 
-  def new
-    @appointment = current_user.appointments.build
-    @appointment.build_client
-  end
-
-  def create
-    # @appointment = current_user.appointments.create(appointment_params)
-    @appointment =  Appointment.new(appointment_params.merge(user_id: current_user.id))
-    if @appointment.save
-      redirect_to appointments_path
-    else
-      render :new
-    end
-  end
-
   def edit
     @appointment = current_user.appointments.find(params[:id])
   end
