10475365430d3909a2eb401f30f328c46c25dcad
John Schlesinger <jschles1@ramapo.edu>
2017-06-07 03:15:11 +0000

    modified datetime validator to validate on update actions

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,7 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :client
 
-  validates_datetime :date, :on => :create, :on_or_after => :today
+  validates_datetime :date, :on => :create, :on => :update, :on_or_after => :today
   validates :date, presence: true
   validates :date, uniqueness: true
 
