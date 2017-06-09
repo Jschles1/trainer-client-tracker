e9639d0aabc53bcf1632c03cd938f5ee63c99cf8
John Schlesinger <jschles1@ramapo.edu>
2017-06-09 23:49:32 +0000

    Created appointment_fields & appointment_field_label view helpers, added appointment_fields to client form partial & appointment_complete form

--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -8,6 +8,20 @@ module ClientHelper
     end
   end
 
+  def appointment_fields(form, client, appointment)
+    if current_page?(new_client_path) || current_page?(appointment_complete_path(client))
+      render partial: "appointment_fields", locals: {f: form, appointments: appointment, client: client}
+    end
+  end
+
+  def appointment_field_label(appointment, client)
+    if current_page?(new_client_path)
+      appointment.label :date, "Schedule First Appointment:"
+    elsif current_page?(appointment_complete_path(client))
+      appointment.label :date, "Schedule Next Appointment:"
+    end
+  end
+
   def client_progress(client)
     if client.progress != 0
       case client.goal
