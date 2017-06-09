3650028f9a4a6c0300f6780f01bc2795a98aaeed
John Schlesinger <jschles1@ramapo.edu>
2017-06-09 19:04:54 +0000

    Created client_progress view helper & added it to client show view

--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -8,6 +8,18 @@ module ClientHelper
     end
   end
 
+  def client_progress(client)
+    if client.weight_histories != nil
+      content_tag(:h3, "Progress:")
+      content_tag(:br)
+      if client.goal == "Lose Weight"
+        content_tag(:h4, "#{client_name(client)} has lost #{client.progress} lbs. since your first appointment.")
+      elsif client.goal == "Gain Weight"
+        content_tag(:h4, "#{client_name(client)} has gained #{client.progress} lbs. since your first appointment.")
+      end
+    end
+  end
+
   def most_dedicated_client(clients)
     clients.most_dedicated.name
   end
