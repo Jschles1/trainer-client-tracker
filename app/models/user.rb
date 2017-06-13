3b95b9053f25bbb742124b6955a335dd2944b658
John Schlesinger <johnschlesinger@Johns-MBP.fios-router.home>
2017-06-13 19:41:19 -0400

    Added from_omniauth class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,16 @@ class User < ApplicationRecord
   # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates :password, length: { minimum: 8 }
   has_secure_password
+
+  def self.from_omniauth(auth)
+    self.where(:uid => auth["uid"]).first_or_create do |user|
+      user.name = auth["info"]["name"]
+      user.email = "#{auth["uid"]}@#{auth["provider"]}.com"
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.password = SecureRandom.hex(8)
+      user.password_confirmation = user.password
+      user.save!
+    end
+  end
 end
