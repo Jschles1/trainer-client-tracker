3217c29b6bc4030b8ea537dea5ac508f3c614533
John Schlesinger <johnschlesinger@Johns-MBP.fios-router.home>
2017-06-13 19:36:03 -0400

    Modified create & destroy actions, removed auth & oauth_login methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,41 +6,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if !params[:provider].nil?
-      oauth_login
-    elsif params[:user][:email].blank? || params[:user][:password].blank?
-      redirect_to login_path, alert: "Log-in failed."
+    if request.env['omniauth.auth'] != nil
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+      redirect_to clients_path
     else
       normal_login
     end
   end
 
   def destroy
-    session.clear
+    session.delete :user_id
     redirect_to login_path
   end
 
   private
-
-  def auth
-    request.env['omniauth.auth']
-  end
-
-  def oauth_login
-    @user = User.find_or_create_by(:uid => auth['uid']) do |u|
-      u.name = auth['info']['name']
-      u.email = auth['info']['email']
-      u.provider = auth['provider']
-      u.oauth_token = auth['credentials']['token']
-      u.oauth_expires_at = auth['credentials']['expires_at']
-    end
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to clients_path
-    else
-      redirect_to login_path, alert: "Log-in failed."
-    end
-  end
 
   def normal_login
     @user = User.find_by(email: params[:user][:email])
