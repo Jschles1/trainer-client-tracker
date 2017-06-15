6adc6bcc93d294f6376f232bdb27092cdd493d96
Jschles1 <jschles1@ramapo.edu>
2017-06-14 22:10:44 -0400

    replaced new_client_path w/ new_user_client_path

--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -9,13 +9,13 @@ module ClientHelper
   end
 
   def appointment_fields(form, client, appointment)
-    if current_page?(new_client_path) || current_page?(appointment_complete_path(client))
+    if current_page?(new_user_client_path(current_user)) || current_page?(appointment_complete_path(client))
       render partial: "appointment_fields", locals: {f: form, appointments: appointment, client: client}
     end
   end
 
   def appointment_field_label(appointment, client)
-    if current_page?(new_client_path)
+    if current_page?(new_user_client_path(current_user))
       appointment.label :date, "Schedule First Appointment:"
     elsif current_page?(appointment_complete_path(client))
       appointment.label :date, "Schedule Next Appointment:"
