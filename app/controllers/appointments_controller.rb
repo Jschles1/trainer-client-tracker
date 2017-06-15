97923689953b1dac885558452d1ad8f79e8010b9
Jschles1 <jschles1@ramapo.edu>
2017-06-14 22:11:50 -0400

    replaced appointments_path w/ user_appointments_path

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -6,17 +6,17 @@ class AppointmentsController < ApplicationController
   def edit
     @client = current_user.clients.find_by(id: params[:client_id])
     if @client.nil?
-      redirect_to appointments_path, alert: "Client not found."
+      redirect_to user_appointments_path(current_user), alert: "Client not found."
     else
       @appointment = @client.appointments.find_by(id: params[:id])
-      redirect_to appointments_path, alert: "Appointment not found." if @appointment.nil?
+      redirect_to user_appointments_path(current_user), alert: "Appointment not found." if @appointment.nil?
     end
   end
 
   def update
     @appointment = current_user.appointments.find_by(id: params[:id])
     if @appointment.update(appointment_params)
-      redirect_to appointments_path
+      redirect_to user_appointments_path(current_user)
     else
       render :edit
     end
