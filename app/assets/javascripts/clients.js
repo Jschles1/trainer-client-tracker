c95df1137121a5a24fa779618373be73e0547a13
Jschles1 <jschles1@ramapo.edu>
2017-08-12 19:14:54 -0400

    Removed loadClients() & loadAppointments(), created Client constructor function, added click event listener that makes jQuery GET request to My Clients link

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -1,11 +1,32 @@
+class Client {
+  constructor(id, name, email, phone, age, weight, goal, completed_appointments, progress) {
+    this.id = id
+    this.name = name
+    this.email = email
+    this.phone = phone
+    this.age = age
+    this.weight = weight
+    this.goal = goal
+    this.completed_appointments = completed_appointments
+    this.progress = progress
+  }
+}
+
 $(function() {
-  
+  $('a.load_clients').on('click', function(e) {
+    e.preventDefault()
+    $('.container').html("")
+    $.get("/clients.json", function(data) {
+      console.log(data)
+    })
+  })
 })
 
-function loadClients() {
-  console.log("This workds")
-}
+// function loadClients() {
+//   $('.container').html("X")
+//   console.log("This workds")
+// }
 
-function loadAppointments() {
-  console.log("This works")
-}
+// function loadAppointments() {
+//   console.log("This works")
+// }
