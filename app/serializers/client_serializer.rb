5f53ac4f771934a68078aa92754f979d7b0b1e04
Jschles1 <jschles1@ramapo.edu>
2017-08-12 19:12:41 -0400

    Added attributes & has_many association to Clients serializer, removed turbolinks from application layout, returned My clients & My appointments back to link_to helpers w/ added classes of load_clients & load_appointments

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,3 +1,4 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :phone, :age, :weight, :goal, :completed_appointments, :progress
+  has_many :appointments
 end
