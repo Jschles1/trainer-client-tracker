cda9f1b5dcd38ba39e06d3a4185abb774bf1485b
Jschles1 <jschles1@ramapo.edu>
2017-08-13 21:31:05 -0400

    Added tableHeader template literal & formatRow Appointment prototype method. We now render the appointments index by constructing a new Appointment object & then through adding the tableHeader template literal & by calling formatRow() on the appointment object. No longer using handlebars.

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,25 +1,45 @@
+var tableHeader = `
+  <table class="table">
+    <tr class="header-row">
+      <th class="column-header">Client:</th>
+      <th class="column-header">Date:</th>
+      <th class="column-header"></th>
+      <th class="column-header"></th>
+    </tr>
+  </table>
+`
+
 class Appointment {
-  constructor(id, user_id, client_id, date) {
+  constructor(id, user_id, client_id, client_name, date) {
     this.id = id
     this.user_id = user_id
     this.client_id = client_id
+    this.client_name = client_name
     this.date = date
   }
+}
+
+Appointment.prototype.formatRow = function() {
+  return `
+    <tr>
+      <td>${this.client_name}</td>
+      <td>${this.date}</td>
+      <td><a href="/clients/${this.client_id}/appointments/${this.id}/edit" class="btn btn-default">Reschedule Appointment</a></td>
+      <td><a href="/clients/${this.client_id}/appointment_complete" class="btn btn-success">Appointment Complete</a></td>
+    </tr>
+  `
 }
 
 $(function() {
   $('a.load_appointments').on('click', function(e) {
     e.preventDefault()
-    $('.index-list').html("")
     $('.index-header').html("Your Appointments:")
+    $('.index-list').html(tableHeader)
     $.get("/appointments.json", function(data) {
-      var template = Handlebars.compile($('#appointments-index-template').html())
-      var results = template(data)
-      $('.index-list').append(results)
-      console.log(data)
-      // data.forEach(c => {
-      //   console.log(c)
-      // })
+      data.forEach(a => {
+        const newAppointment = new Appointment(a.id, a.user_id, a.client_id, a.client.name, a.date)
+        $('tbody').append(newAppointment.formatRow())
+      })
     })
   })
 })
