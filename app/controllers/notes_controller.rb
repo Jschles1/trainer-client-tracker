6582eba545e0d8075855b0bdf02c2be70b554834
Jschles1 <jschles1@ramapo.edu>
2017-08-15 20:32:00 -0400

    Added note_params private method, fixed typo @ line 7, added 201 status code to specify created resource

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,10 +4,16 @@ class NotesController < ApplicationController
   end
 
   def create
-    @note = note.new(note_params)
+    @note = Note.new(note_params)
     if @note.save
-      render json: @note
+      render json: @note, status: 201
     end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:text, :client_id)
   end
 
 end
