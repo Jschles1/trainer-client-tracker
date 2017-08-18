2af84fd14b1c7f5b6818d29d49ce70d13c5616a7
Jschles1 <jschles1@ramapo.edu>
2017-08-17 23:57:01 -0400

    Modified create action & normal_login to redirect to dashboard on successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if request.env['omniauth.auth'] != nil
       @user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = @user.id
-      redirect_to clients_path
+      redirect_to dashboard_path
     else
       normal_login
     end
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
       @user = User.find_by(email: params[:user][:email])
       if !!@user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        redirect_to clients_path
+        redirect_to dashboard_path
       else
         redirect_to login_path, alert: "Log-in failed."
       end
