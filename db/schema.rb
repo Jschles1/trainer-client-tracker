dd655fc3506054e3a285fd4075ae9f029baad868
Jschles1 <jschles1@ramapo.edu>
2017-11-28 20:29:02 -0500

    Changed oauth_token & password_digest data types to text from string

--- db/schema.rb
+++ db/schema.rb
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 20170812003157) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
+    t.text     "password_digest"
     t.string   "uid",              limit: 8
     t.string   "provider"
-    t.string   "oauth_token"
+    t.text     "oauth_token"
     t.datetime "oauth_expires_at"
   end
 
